d6b6b1f1acec65f5dc02323cab55353bffcf58cb
Alejandro Núñez Arroyo <50973610+AlejandroNunezArroyo@users.noreply.github.com>
2021-10-26 12:46:35 -0400

    Update camera_fun.py

--- env_camera/camera_fun.py
+++ env_camera/camera_fun.py
@@ -5,7 +5,8 @@ import numpy as np
 vid = cv2.VideoCapture(0)
 
 detector = HomogeneousBgDetector()
-while(True):
+bool_v = True
+while(bool_v):
       
     # Capture the video frame
     # by frame
@@ -17,24 +18,28 @@ while(True):
     circles_im = np.copy(frame)
 
     # draw each one
-    circles = detector.circle_detector(frame)
-    for circle in circles[0,:]:
-        # draw the outer circle
-        cv2.circle(circles_im,(circle[0],circle[1]),circle[2],(0,255,0),2)
-        # draw the center of the circle
-        cv2.circle(circles_im,(circle[0],circle[1]),2,(0,0,255),3)
-    
-    cv2.imshow('circles_im', circles_im)   
-    print('Circles shape: ', circles.shape)
+    try:
+        circles = detector.circle_detector(frame)
 
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
+        for circle in circles[0,:]:
+            # draw the outer circle
+            cv2.circle(circles_im,(circle[0],circle[1]),circle[2],(0,255,0),2)
+            # draw the center of the circle
+            cv2.circle(circles_im,(circle[0],circle[1]),2,(0,0,255),3)
+        
+        cv2.imshow('circles_im', circles_im)   
+        print('Circles shape: ', circles.shape)
 
-    # the 'q' button is set as the
-    # quitting button you may use any
-    # desired button of your choice
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+
+        # the 'q' button is set as the
+        # quitting button you may use any
+        # desired button of your choice
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    except:
+        bool_v
   
 # After the loop release the cap object
 vid.release()
